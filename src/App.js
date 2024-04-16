585978427eaa5d62ea3d5b92cb535c815c927ad9
Adam Mulani <aadammulani12@gmail.com>
2024-04-16 15:41:29 +0530

    Fetched and displayed posts data from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Header } from "./components/Header";
 import { Blogs } from "./components/Blogs";
 import { Pagination } from "./components/Pagination";
+import { AppContext } from "./context/AppContext";
+import "./App.css"
 
 export default function App() {
+
+  const {fetchBlogPosts}=useContext(AppContext);
+
+  useEffect(()=>{
+    fetchBlogPosts();
+  },[])
   return <div>
     <Header/>
     <Blogs/>
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,7 +1,41 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { Spinner } from './Spinner.js';
 export const Blogs = () => {
+
+    const {posts,loading}=useContext(AppContext);
+
   return (
-    <div>Blogs</div>
+    <div>
+        {
+            loading ? <Spinner/>:
+            (
+                posts.length===0?(
+                    <div>
+                        <p>No post found</p>
+                    </div>
+                ):
+                (
+                    posts.map((post)=>(
+
+                        <div key={post.id}>
+                            <p className='font-bold'>{post.title}</p>
+                            <p>
+                                By <span>{post.author}</span> on <span>{post.category}</span>
+                            </p>
+                            <p>Posted on {post.date}</p>
+                            <p>{post.content}</p>
+                            <div>
+                                {post.tags.map((tag,index)=>{
+                                    return <span key={index}>{`#${tag} `}</span>
+                                })}
+                            </div>
+                        </div>
+
+                    ))
+                )
+            )
+        }
+    </div>
   )
 }
